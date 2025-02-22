a267a542047d54c7772f06f28bd1ac3bce490d4f
Zakaria <ZKAC403@rhul.ac.uk>
2025-02-22 23:52:30 +0000

    feat- bot now outputs any one of the 40 hadith at random

--- main.py
+++ main.py
@@ -1,21 +1,26 @@
 import os
-
 import discord
 from dotenv import load_dotenv
 from discord.ext import commands
+import asyncio
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 
+# Bot setup
 intents = discord.Intents.default()
 intents.message_content = True
-
-#client = discord.Client(intents=intents)
 bot = commands.Bot(command_prefix="!", intents=intents)
 
-@bot.command()
-async def ping(ctx):
-    await ctx.send("Pong!")
+@bot.event
+async def on_ready():
+    print(f'Logged in as {bot.user}')
+    try:
+        await bot.load_extension("cogs.reminder")
+        print("Reminder cog loaded!")
+    except Exception as e:
+        print(f"Error loading reminder cog: {e}")
 
-
-bot.run(TOKEN)
+# Run the bot
+if __name__ == "__main__":
+    bot.run(TOKEN)
