78bb8096a42665a8f1db8a282ea84aced2d436c9
Danyal Shah <zlac208@live.rhul.ac.uk>
2025-02-22 11:08:42 +0000

    Restructure main.py

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import os
 
 import discord
 from dotenv import load_dotenv
+from discord.ext import commands
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
@@ -9,20 +10,26 @@ TOKEN = os.getenv('DISCORD_TOKEN')
 intents = discord.Intents.default()
 intents.message_content = True
 
-client = discord.Client(intents=intents)
+#client = discord.Client(intents=intents)
+bot = commands.Bot(command_prefix="!", intents=intents)
 
-@client.event
+# @client.event
+@bot.event
 async def on_ready():
     print(f'{client.user} has connected to Discord!')
 
 
-@client.event
-async def on_message(message):
-    if message.author == client.user:
-        return
+# @client.event
+# async def on_message(message):
+#     if message.author == client.user:
+#         return
 
-    if message.content.startswith('!hello'):
-        await message.channel.send('Hello!')
+#     if message.content.startswith('!hello'):
+#         await message.channel.send('Hello!')
+
+@bot.command()
+async def ping(ctx):
+    await ctx.send("Pong!")
 
 
-client.run(TOKEN)
+bot.run(TOKEN)
