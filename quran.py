02dd96e5cdcfcedf48a523cf7749ab3461d583df
Danyal Shah <zlac208@live.rhul.ac.uk>
2025-02-22 19:51:55 +0000

    Get Surah Needs to be rewritten for longer surahs such as al-baqarah

--- quran.py
+++ quran.py
@@ -8,6 +8,18 @@ class Quran(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
+        with open("data/quran.json", encoding='utf-8') as quran:
+            self.quran = json.load(quran)
+
+        with open("data/en.json", encoding='utf-8') as en:
+            self.quran_en = json.load(en)
+
+        with open("data/transliteration.json", encoding='utf-8') as transliteration:
+            self.transliteration = json.load(transliteration)
+
+        with open("data/chapters.json", encoding='utf-8') as chapters:
+            self.chapters = json.load(chapters)
+
     @commands.command()
     async def randomVerse(self, ctx):
         pass
@@ -19,3 +31,11 @@ class Quran(commands.Cog):
     @commands.command()
     async def listenToVerse(self, ctx):
         pass
+
+    @commands.command()
+    async def surah(self, ctx, surah_num):
+        name = self.chapters[int(surah_num)-1]["name"]
+        name_trans = self.chapters[int(surah_num)-1]["transliteration"]
+        await ctx.send(f"# __{name} - {name_trans}__")
+        for verse in self.quran[surah_num]:
+            await ctx.send(f"## ﴾{verse["verse"]}﴿ - {verse["text"]}")
