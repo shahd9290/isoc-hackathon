8a0c4c097675d74452f4c8ec2a4ad44b52bf4121
Danyal Shah <zlac208@live.rhul.ac.uk>
2025-02-22 13:51:39 +0000

    Created Questions Cog

--- main.py
+++ main.py
@@ -5,13 +5,19 @@ from dotenv import load_dotenv
 from discord.ext import commands
 
 from test import Test
+from questions import Questions
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 
 intents = discord.Intents.default()
 intents.message_content = True
+
 bot = commands.Bot(command_prefix="!", intents=intents)
+
+async def load_cogs():
+    await bot.add_cog(Test(bot))  # Now we await it inside an async function
+    await bot.add_cog(Questions(bot))
 
 @bot.event
 async def on_ready():
