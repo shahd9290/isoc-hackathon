9db50ccd674bca48b874ad890b3f212d642c6573
Danyal Shah <zlac208@live.rhul.ac.uk>
2025-02-22 11:22:00 +0000

    addNum function

--- main.py
+++ main.py
@@ -10,26 +10,20 @@ TOKEN = os.getenv('DISCORD_TOKEN')
 intents = discord.Intents.default()
 intents.message_content = True
 
-#client = discord.Client(intents=intents)
 bot = commands.Bot(command_prefix="!", intents=intents)
-
-# @client.event
-@bot.event
-async def on_ready():
-    print(f'{client.user} has connected to Discord!')
-
-
-# @client.event
-# async def on_message(message):
-#     if message.author == client.user:
-#         return
-
-#     if message.content.startswith('!hello'):
-#         await message.channel.send('Hello!')
 
 @bot.command()
 async def ping(ctx):
     await ctx.send("Pong!")
 
+@bot.command()
+async def addNum(ctx, *args):
+    try:
+        sum = 0
+        for i in args:
+            sum += int(i)
+        await ctx.send(sum)
+    except:
+        await ctx.send("Detected Strings!")
 
 bot.run(TOKEN)
