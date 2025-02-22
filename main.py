6c4d0ad98994385bea96803b38f0c4611212bdb8
Danyal Shah <zlac208@live.rhul.ac.uk>
2025-02-22 10:37:15 +0000

    Hello Command

--- main.py
+++ main.py
@@ -7,11 +7,22 @@ load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 
 intents = discord.Intents.default()
+intents.message_content = True
 
 client = discord.Client(intents=intents)
 
 @client.event
 async def on_ready():
     print(f'{client.user} has connected to Discord!')
+
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content.startswith('$hello'):
+        await message.channel.send('Hello!')
+
 
 client.run(TOKEN)
