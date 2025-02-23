15eb72723c1df3dd0a6bfd306c952eb3763b4495
Danyal Shah <zlac208@live.rhul.ac.uk>
2025-02-23 11:36:12 +0000

    Timeout for quiz, modified questions.json

--- questions.py
+++ questions.py
@@ -1,3 +1,5 @@
+import asyncio.exceptions
+
 from discord.ext import commands
 import discord
 import json
@@ -33,7 +35,7 @@ class Questions(commands.Cog):
             await ctx.send("Great!")
             score = 0
             # load question
-            with open("data/data.json", "r") as file:
+            with open("data/questions.json", "r") as file:
                 data = json.load(file)
             # loop through questions
             for questionJson in data["quiz"]:
@@ -44,6 +46,7 @@ class Questions(commands.Cog):
                     desc += f"{i+1}. {options[i]}\n"
                     options[i] = options[i].lower()
                 embed = discord.Embed(title=question, colour=discord.Colour.purple(), description=desc)
+                embed.set_footer(text="You can end the quiz at any time by sending \"end\" in the chat")
                 answer = questionJson["answer"].lower()
                 ans_index = options.index(answer) + 1
                 await ctx.send(embed=embed)
@@ -51,11 +54,18 @@ class Questions(commands.Cog):
                 # validation to make sure option is in options.
                 while True:
                 # wait for user input
-                    ans = await self.bot.wait_for("message", check=check, timeout=30.0)
-                    if ans.content.lower() in options or ans.content in ["1", "2", "3", "4"]:
-                        break
-                    else:
-                        await ctx.send("Invalid Option! Please enter the answer or corresponding number.")
+                    try:
+                        ans = await self.bot.wait_for("message", check=check, timeout=30.0)
+                        if ans.content.lower() in options or ans.content in ["1", "2", "3", "4"]:
+                            break
+                        elif ans.content.lower() == "end":
+                            await ctx.send("Ending the quiz!")
+                            return
+                        else:
+                            await ctx.send("Invalid Option! Please enter the answer or corresponding number.")
+                    except asyncio.exceptions.TimeoutError:
+                        await ctx.send("Didn't receive an answer in time! Ending the quiz")
+                        return
 
                 # check if answer matches answer string or index + 1
                 if ans.content.lower() == answer or ans.content == str(ans_index):
